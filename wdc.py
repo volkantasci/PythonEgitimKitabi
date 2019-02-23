264dfebfb70976aa71a21445c043cc73ee8fe005
Volkan Taşcı <proprogramci@outlook.com>
2019-02-23 20:43:13 +0300

    wdc dosyası düzenlendi

--- wdc.py
+++ wdc.py
@@ -1,96 +1,103 @@
 #!/usr/bin/python
 
 
-from os.path import isdir, isfile, islink
+from os.path import isdir, isfile, islink, exists
 from sys import argv, exit
-
 
 class WordCounter:
     @staticmethod
     def check_arguments(option):
-        allowed_args = '-l', '-w', '-c', '--help'
+        allowed_args = '-l','-w','-c','--help'
         if option in allowed_args:
             return True
         else:
             return False
-
+    
+    
     @staticmethod
     def help_page():
         msg = """
         Word Counter -- Help Page
-
+        
     Word Counter, dosyalarınızda kaç satır, kelime ve karakter olduğunu gösteren, Python ile kodlanmış bir programdır.
 
-
+        
         -- wdc [OPTION] [FILE]
-
+        
         Examples:
         wdc -l example.txt
         wdc -w example.txt
         wdc -c example.txt
         wdc example.txt
-
-
+        
+        
         -l:\t Dosyadaki satır sayısını göster
         -w:\t Dosyadaki kelime sayısını göster
         -c:\t Dosyadaki karakter sayısını göster
-
+        
     Herhangi bir parametre kullanılmadığında tüm bilgiler gösterilir.
-
+        
         """
         return msg
-
+    
+    
     @staticmethod
     def check_file(file):
+        if not exists(file):
+            return 4
+        
         if isfile(file):
             return 1
-
-        elif isdir(file):
+        
+        if isdir(file):
             return 2
-
-        elif islink(file):
+        
+        if islink(file):
             return 3
+        
 
-        else:
-            return "Unknown"
-
+        
+    
     @staticmethod
     def line_count(file=None, data=None):
         if not data:
             # Eğer ki verilmiş bir data yoksa
             with open(file) as f:
                 data = f.read()
-
+        
         l = data.count('\n') + 1
-
+        
         if data[-1] == '\n':
             l -= 1
-
+        
         return l
-
+    
+    
     @staticmethod
     def word_count(file=None, data=None):
         if not data:
             # Eğer ki verilmiş bir data yoksa
             with open(file) as f:
                 data = f.read()
-
+        
         data = data.replace('\n', ' ')
         w = len(data.split(' '))
-
+        
         return w
-
+    
+    
     @staticmethod
     def char_count(file=None, data=None):
         if not data:
             # Eğer ki verilmiş bir data yoksa
             with open(file) as f:
                 data = f.read()
-
+        
         c = len(data)
-
+        
         return c
-
+    
+    
 
 def main():
     if len(argv) == 2:
@@ -99,32 +106,33 @@ def main():
         if file_or_arg == '--help':
             print(WordCounter.help_page())
             exit()
-
+            
+        
         if WordCounter.check_file(file_or_arg) == 1:
             # Print All Options
-            with open(file_or_arg) as f:
+            with open(file) as f:
                 data = f.read()
-
+                
             result = " {} {} {}  :'{}'".format(WordCounter.line_count(data=data),
-                                               WordCounter.word_count(data=data),
-                                               WordCounter.char_count(data=data),
-                                               file_or_arg)
-
+                                              WordCounter.word_count(data=data),
+                                              WordCounter.char_count(data=data),
+                                              file_or_arg)
+            
             print(result)
-
+        
         elif WordCounter.check_file(file_or_arg) == 2:
             print("Bu bir dizin!")
             exit()
-
+        
         elif WordCounter.check_file(file_or_arg) == 3:
             print("Bu bir link!")
             exit()
-
-        else:
-            print("Biçim tanınmıyor!")
+            
+        else: # file_or_arg = 4
+            print("Dosya mevcut değil!")
             exit()
-
-
+            
+    
     elif len(argv) == 3:
         # Have One Argument
         file = argv[2]
@@ -132,34 +140,35 @@ def main():
         if WordCounter.check_arguments(option) and WordCounter.check_file(file):
             if option == '-l':
                 result = " {}  :'{}'".format(WordCounter.line_count(file=file),
-                                             file)
-
+                                            file)
+                
                 print(result)
-
+            
             elif option == '-w':
                 result = " {}  :{}".format(WordCounter.word_count(file=file),
-                                           file)
-
+                                          file)
+                
                 print(result)
-
+            
             elif option == '-c':
                 result = " {}  :{}".format(WordCounter.char_count(file=file),
-                                           file)
-
+                                          file)
+                
                 print(result)
-
+            
             else:
                 print(WordCounter.help_page())
-
+        
         else:
             print(WordCounter.help_page())
-
+    
     else:
         print(WordCounter.help_page())
-
+            
+            
 
 if __name__ == '__main__':
     main()
-
+    
 else:
     pass
